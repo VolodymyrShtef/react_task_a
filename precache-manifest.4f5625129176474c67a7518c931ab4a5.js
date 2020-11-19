6a61dc0719c77ebd78aafc129944a83ff9d6e1c9
VolodSh <vzujka.com.ua@gmail.com>
2020-11-19 23:28:14 +0200

    Updates

---
+++ precache-manifest.4f5625129176474c67a7518c931ab4a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ef3e7053b4d4a9bbe1bf907d30b0ee8",
+    "url": "/react_task_a/index.html"
+  },
+  {
+    "revision": "de1a6834c6e03270d129",
+    "url": "/react_task_a/static/css/main.40e9d355.chunk.css"
+  },
+  {
+    "revision": "0bed835c0144a2fea780",
+    "url": "/react_task_a/static/js/2.a853e744.chunk.js"
+  },
+  {
+    "revision": "de1a6834c6e03270d129",
+    "url": "/react_task_a/static/js/main.99d48eae.chunk.js"
+  },
+  {
+    "revision": "1f2cf780066ca6e9e6cc",
+    "url": "/react_task_a/static/js/runtime-main.f0d9b772.js"
+  }
+]);
